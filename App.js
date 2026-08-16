b2365ff373dee6c5b3345daeec54b090cce603c7
yanshuaibi <3527870977@qq.com>
2026-08-16 18:59:04 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import RootNavigation from './src/navigation/RootNavigation';
+
+export default function App() {
+    return (
+        <>
+            <RootNavigation />
+            <StatusBar style="dark" />
+        </>
+    );
+}
