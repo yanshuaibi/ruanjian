cd4aa26d35b684d51f2ccf73c732052f23281d44
ymjlz123580 <2180692469@qq.com>
2026-08-25 19:54:12 +0800

    hide status bar and remove fake search bar

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ export default function App() {
     return (
         <>
             <RootNavigation />
-            <StatusBar style="dark" />
+            <StatusBar hidden={true} />
         </>
     );
 }
